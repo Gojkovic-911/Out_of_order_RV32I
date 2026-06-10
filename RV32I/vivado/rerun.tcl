7c4839aabfb942798227ce0262b3441ca4a4d049
Filip <Filip>
2026-06-10 09:52:49 +0200

    rerun.tcl edited

--- RV32I/vivado/rerun.tcl
+++ RV32I/vivado/rerun.tcl
@@ -4,7 +4,7 @@
 
 # 1. Run the bash script for binary conversion (or any pre-simulation setup)
 puts "Running convert_bin.sh..."
-exec bash convert_bin.sh
+exec bash ../scripts/convert_bin.sh
 
 # 2. Relaunch the simulation (loads the updated design)
 puts "Relaunching simulation..."
@@ -12,10 +12,10 @@ relaunch_sim
 run 50 us
 
 puts "Running reg_dump.tcl..."
-source reg_dump.tcl
+source ../scripts/print_current_state.tcl
 
 # 3. Run the comparison script (to verify results after simulation)
 puts "Running compare_script.sh..."
-exec bash compare_script.sh
+exec bash ../scripts/compare_script.sh
 
 puts "All scripts completed."
